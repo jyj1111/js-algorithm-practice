727726358d38400616f085a8b14947f3f9e3cb93
jyj <jyj202040@gmail.com>
2022-08-04 11:10:41 +0900

    2 solve

---
+++ dreamcoding/async/timeout.js
@@ -0,0 +1,11 @@
+function runInDelay(callback, seconds) {
+  if (seconds < 0) {
+    throw new Error("seconds는 0보다 커야 함");
+  } else {
+    setTimeout(callback, seconds * 1000);
+  }
+}
+
+runInDelay(() => {
+  console.log("타이머 완료");
+}, -1);
