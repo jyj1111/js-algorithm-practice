6198a7d5c77d71100aab172d3d3b632abba7da8c
jyj <jyj202040@gmail.com>
2022-08-13 22:41:38 +0900

    2 solve

--- inflearn/section8/6.js
+++ inflearn/section8/6.js
@@ -1,15 +1,23 @@
-function sol(c, arr) {
+function sol(m, arr) {
   let answer = 0;
-  function DFS(L, sum) {
-    if (sum > c) return;
+  function DFS(L, point, time) {
+    if (time > m) return;
     if (L === arr.length) {
-      answer = Math.max(answer, sum);
+      answer = Math.max(answer, point);
     } else {
-      DFS(L + 1, sum + arr[L]);
-      DFS(L + 1, sum);
+      DFS(L + 1, point + arr[L][0], time + arr[L][1]);
+      DFS(L + 1, point, time);
     }
   }
-  DFS(0, 0);
+  DFS(0, 0, 0);
   return answer;
 }
-console.log(sol(259, [81, 58, 42, 33, 61]));
+console.log(
+  sol(20, [
+    [10, 5],
+    [25, 12],
+    [15, 8],
+    [6, 3],
+    [7, 4],
+  ])
+);
